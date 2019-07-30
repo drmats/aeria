a596e41e4bea9f730681ab851c159c6441ddf257
drmats <drmats@users.noreply.github.com>
2019-07-30 17:47:24 +0200

    Config: app skeleton - babel, webpack, eslint, build-and-run scripts.

---
+++ src/index.js
@@ -0,0 +1,22 @@
+/**
+ * Aeria.
+ *
+ * Flightlog.
+ *
+ * @module aeria-app
+ * @license Apache-2.0
+ */
+
+
+
+
+import {
+    name as applicationName,
+    version,
+} from "../package.json"
+
+
+
+
+// eslint-disable-next-line no-console
+console.info(`${applicationName} v.${version}`)
---
+++ webpack.config.js
@@ -0,0 +1,93 @@
+"use strict"
+
+
+
+
+// ...
+const
+    fs = require("fs"),
+    path = require("path"),
+    webpack = require("webpack"),
+    MinifyPlugin = require("babel-minify-webpack-plugin"),
+    appDirectory = fs.realpathSync(process.cwd()),
+    nodeExternals = require("webpack-node-externals")
+
+
+
+
+// ...
+module.exports = {
+
+    mode: "production",
+
+
+    target: "node",
+
+
+    externals: [nodeExternals()],
+
+
+    entry: {
+        "aeria": path.resolve(
+            appDirectory, "src/index.js"
+        ),
+    },
+
+
+    output: {
+        filename: "[name].js",
+        chunkFilename: "[name].c.js",
+        path: path.resolve(__dirname, "./dist"),
+        libraryTarget: "commonjs",
+        globalObject: "this",
+    },
+
+
+    optimization: {
+        minimize: true,
+        mergeDuplicateChunks: true,
+        sideEffects: true,
+        providedExports: true,
+        concatenateModules: true,
+        occurrenceOrder: true,
+        removeEmptyChunks: true,
+        removeAvailableModules: true,
+        minimizer: [
+            new MinifyPlugin({}, {
+                comments: false,
+            }),
+        ],
+    },
+
+
+    module: {
+        rules: [
+            {
+                enforce: "pre",
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: "eslint-loader",
+            },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: "babel-loader",
+                sideEffects: false,
+            },
+        ],
+    },
+
+
+    node: {
+        __dirname: false,
+        __filename: false,
+    },
+
+
+    plugins: [
+        new webpack.DefinePlugin({
+            "process.env.BABEL_ENV": JSON.stringify("production"),
+        }),
+    ],
+
+}
